4f1ce1118a6fcab2edf7939f3664698847790e6d
Marcos Vinicius <mvsouza7@gmail.com>
2022-02-01 21:03:35 +0200

    docs: first files added!

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const FormInput = () => {
+  return <div className="formInput">
+      <label>Username</label>
+      <input type="text" placeholder="Username" />
+  </div>;
+};
+
+export default FormInput;
