74dac325d784de27cb0e96239e5298151d1725c6
Marcos Vinicius <mvsouza7@gmail.com>
2022-02-02 19:38:15 +0200

    docs: validation added, with css style to show if the validations are correct ou no for the user. project done!

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import "../FormInput.css";
 
 const FormInput = (props) => {
-  const { label, onChange, id, ...InputProps} = props;
+  const [focused, setFocused] = useState(false)
+  const { label, errorMessage, onChange, id, ...InputProps} = props;
+
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...InputProps} onChange={onChange}/>
+      <input {...InputProps} 
+      onChange={onChange} 
+      onBlur={handleFocus} 
+      onFocus={() => inputProps.name === "confirmPassword" && setFocused(true)}
+      focused={focused.toString()}/>
+      <span>{errorMessage}</span>
     </div>
   );
 };
